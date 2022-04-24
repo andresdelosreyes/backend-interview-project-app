32d97239cbabb96433cfafa2dcb5e158258e61d5
Andres de los Reyes <andresdelosreyes@gmail.com>
2022-04-24 17:19:19 -0500

    Add pitest for mutation testing

--- src/main/java/com/ninjaone/backendinterviewproject/database/repository/SampleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ninjaone.backendinterviewproject.database.repository;
-
-import com.ninjaone.backendinterviewproject.database.model.Sample;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SampleRepository extends CrudRepository<Sample, String> {}
